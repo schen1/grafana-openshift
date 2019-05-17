adac7e20276ecb2856e528929a294e1136f5af16
Sylvain Chen <sychen@redhat.com>
2019-05-17 13:09:12 +0200

    add route and fix config

--- set-up.sh
+++ set-up.sh
@@ -2,9 +2,7 @@
 
 oc create -f openshift/configmaps.yaml
 oc create -f openshift/pvc.yaml
-oc create -f openshift/svc.yaml
 oc create -f openshift/sa.yaml
 oc create -f openshift/dc.yaml
-
-## Maybe add some roles to SA
-## Expose Route
+oc create -f openshift/svc.yaml
+oc create -f openshift/route.yaml
